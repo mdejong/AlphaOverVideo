816dd4ee3da49a965d0f3235dedcdc9af9dc3afb
Mo DeJong <mo@modejong.com>
2019-04-04 22:10:51 -0700

    init CarSpinAlpha in default new swift project state

---
+++ CarSpinAlpha/CarSpinAlpha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarSpinAlpha
+//
+//  Created by Mo DeJong on 4/4/19.
+//  Copyright © 2019 HelpURock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
