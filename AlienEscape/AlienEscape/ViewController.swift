998ab80b935172b6808052edbf3d129f55c255a9
Mo DeJong <mo@modejong.com>
2019-04-08 00:49:31 -0700

    add view that loops alien life form effect, background is black behind transparent chains, import AlphaOverVideo

--- AlienEscape/AlienEscape/ViewController.swift
+++ AlienEscape/AlienEscape/ViewController.swift
@@ -7,10 +7,14 @@
 //
 
 import UIKit
+import AlphaOverVideo
 
 class ViewController: UIViewController {
   
   @IBOutlet weak var bgImageView: UIImageView!
+  @IBOutlet weak var mtkView: AOVMTKView!
+  
+  var player: AOVPlayer?
   
   override var prefersStatusBarHidden: Bool {
     return true
@@ -20,6 +24,33 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     assert(bgImageView != nil)
+    assert(mtkView != nil)
+    
+    mtkView.device = MTLCreateSystemDefaultDevice()
+    
+    if (mtkView.device == nil)
+    {
+      NSLog("Metal is not supported on this device");
+      return;
+    }
+    
+    // Generate RGBA pair or urls to seamless loop an infinite number of times
+    let url1 = AOVPlayer.url(fromAsset:"Field.m4v")
+    let url2 = AOVPlayer.url(fromAsset:"Field_alpha.m4v")
+    assert(url1 != nil)
+    assert(url2 != nil)
+    let clips = [ [ url1, url2 ] ]
+    
+    let player = AOVPlayer.init(loopedClips:clips)
+    self.player = player
+    
+    let worked = mtkView.attach(player)
+    if (worked == false)
+    {
+      NSLog("attach failed for AOVMTKView");
+      return;
+    }
+
   }
 }
 
---
+++ AlienEscape/AlphaOverVideo.framework/Headers/AlphaOverVideo.h
@@ -0,0 +1,23 @@
+//
+//  AlphaOverVideo.h
+//  AlphaOverVideo
+//
+//  Created by Mo DeJong on 4/4/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+@import MetalKit;
+
+//! Project version number for AlphaOverVideo.
+FOUNDATION_EXPORT double AlphaOverVideoVersionNumber;
+
+//! Project version string for AlphaOverVideo.
+FOUNDATION_EXPORT const unsigned char AlphaOverVideoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlphaOverVideo/PublicHeader.h>
+
+#import <AlphaOverVideo/AOVFrame.h>
+#import <AlphaOverVideo/AOVPlayer.h>
+#import <AlphaOverVideo/AOVMTKView.h>
+#import <AlphaOverVideo/AOVDisplayLink.h>
+#import <AlphaOverVideo/H264Encoder.h>
