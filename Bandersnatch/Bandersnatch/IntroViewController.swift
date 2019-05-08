22dacff6790d27f6ca5bee18f562e327df17b94d
Mo DeJong <mo@modejong.com>
2019-05-07 19:23:42 -0700

    create video view and show it when leaving splash screen, link in storyboards

--- Bandersnatch/Bandersnatch/IntroViewController.swift
+++ Bandersnatch/Bandersnatch/IntroViewController.swift
@@ -6,13 +6,45 @@
 //
 
 import UIKit
+import AlphaOverVideo
 
 class IntroViewController: UIViewController {
   
+  @IBOutlet weak var mtkView: AOVMTKView!
+  var player: AOVPlayer?
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    assert(mtkView != nil)
 
     print("IntroViewController.viewDidLoad")
+    
+    mtkView.device = MTLCreateSystemDefaultDevice()
+    
+    if (mtkView.device == nil)
+    {
+      NSLog("Metal is not supported on this device");
+      return;
+    }
+
+    let url1 = AOVPlayer.url(fromAsset:"ClipIntro")
+    assert(url1 != nil)
+    let clips = [ url1 ]
+    
+    let player = AOVPlayer.init(loopedClips:clips)
+    self.player = player
+    
+    // Defaults to sRGB, so set BT.709 flag to indicate video encoding
+    player?.decodeGamma = MetalBT709GammaApple;
+    
+    let worked = mtkView.attach(player)
+    if (worked == false)
+    {
+      NSLog("attach failed for AOVMTKView");
+      return;
+    }
+
   }
 
 }
