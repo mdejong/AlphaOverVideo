bc04cdab2f73a96bd192751aaba747284ad6d80f
Mo DeJong <mo@modejong.com>
2019-05-16 18:07:51 -0700

    add copy_framework.sh script as method to update binary files in build directories

---
+++ AlphaOverVideo/CopyFramework/copy_framework.sh
@@ -0,0 +1,20 @@
+# Run from AlphaOverVideo/AlphaOverVideo/CopyFramework subdirectory, note
+# that AlphaOverVideo.framework must be copied from the build generation
+# dir into this directory first. (it will not be checked into git)
+
+cd ../..
+
+SRC=AlphaOverVideo/AlphaOverVideo/CopyFramework/AlphaOverVideo.framework
+
+rm -rf AlienEscape/AlphaOverVideo.framework
+cp -R ${SRC} AlienEscape
+
+rm -rf CarSpinAlpha/AlphaOverVideo.framework
+cp -R ${SRC} CarSpinAlpha
+
+rm -rf Fireworks/AlphaOverVideo.framework
+cp -R ${SRC} Fireworks
+
+rm -rf Bandersnatch/AlphaOverVideo.framework
+cp -R ${SRC} Bandersnatch
+
